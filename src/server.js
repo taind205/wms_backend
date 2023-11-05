6a4d4a43bd3560f7c4935ebbfaade0759c78f829
Tai Nguyen <taind205@gmail.com>
2023-11-06 01:25:42 +0700

    fix CORS

--- src/server.js
+++ src/server.js
@@ -9,12 +9,12 @@ require("dotenv").config();
 
 const app=express();
 const PORT = process.env.PORT || 8080;
-const ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000';
+const ORIGIN =  [process.env.CORS_ORIGIN, 'http://localhost:3000'];
 
 var corsOptions = {
-    origin: ORIGIN,
+    origin: true,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    credentials: true
+    credentials: true,
   }
 
 //config view engine
